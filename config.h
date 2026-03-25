c368f6ff731332ca50203c613e92d9e8d053185f
dnastro <dnastro10@gmail.com>
2026-03-25 08:40:45 -0300

    Adiciona arquivo config.h com funções de configuração de cheat

---
+++ config.h
@@ -0,0 +1,8 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+
+// Funções para salvar e carregar configurações de cheat
+void save_cheat_configuration(const char* filename);
+void load_cheat_configuration(const char* filename);
+
+#endif // CONFIG_H
