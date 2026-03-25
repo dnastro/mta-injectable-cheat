43b70f80ff04a8a3552f11131a752818edef73d5
dnastro <dnastro10@gmail.com>
2026-03-25 08:26:04 -0300

    Add gui.h with GUI interface functions

---
+++ gui.h
@@ -0,0 +1,13 @@
+#ifndef GUI_H
+#define GUI_H
+
+#include <imgui.h>
+
+// Funções de inicialização da interface
+void InitMenu();
+void DrawMenu();
+
+// Função para limpar a interface
+void ClearMenu();
+
+#endif // GUI_H
